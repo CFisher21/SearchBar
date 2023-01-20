d1d13a36f483819d96039c2fba55a9ab2e809b65
Cody Fisher <95709270+CFisher21@users.noreply.github.com>
2023-01-20 18:50:48 -0330

    Now works with the HTML select
    updated to work with the HTML <select> so the user can select to search the database by first name, last name, email or occupation. This gives the search another layer of accuracy.

--- server/database/pg.dal.js
+++ server/database/pg.dal.js
@@ -1,11 +1,30 @@
 const dal = require('./pgdb')
 
-var getSearch = async function(search) {
-     const sql = "SELECT * FROM mock_data WHERE first_name LIKE  INITCAP($1) or last_name LIKE INITCAP($1) or email LIKE $1 or occupation LIKE INITCAP($1)"
+var getSearch = async function(search, searchby) {
+
      // the line below is to take the search term and build it into "search%", this makes the user not need to input the EXACT name/email/occupation to get a result.
      const fuzzy = `${search}%`
+    if(searchby == "firstname") {
+     const sql = "SELECT * FROM mock_data WHERE first_name LIKE  INITCAP($1)"
      const results = await dal.query(sql, [fuzzy]);
      return results.rows;
+
+    } else if (searchby == "lastname") {
+     const sql = "SELECT * FROM mock_data WHERE last_name LIKE INITCAP($1)"
+     const results = await dal.query(sql, [fuzzy]);
+     return results.rows;
+
+    } else if (searchby == "email") {
+     const sql = "SELECT * FROM mock_data WHERE email LIKE $1"
+     const results = await dal.query(sql, [fuzzy]);
+     return results.rows;
+
+    } else {
+     const sql = "SELECT * FROM mock_data WHERE occupation LIKE INITCAP($1)"
+     const results = await dal.query(sql, [fuzzy]);
+     return results.rows;
+}
+
 }
 
 module.exports = {getSearch}
