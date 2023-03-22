8b0602cc804d00a9f285dbe747c43871e470ec15
Cody Fisher <95709270+CFisher21@users.noreply.github.com>
2023-03-22 00:47:40 -0600

    Updated database file to work with Google Cloud
    Hosted the PostgreSQL server on google cloud and this file is changed so that it can connect to that one instead of the locally hosted one.

--- server/database/pgdb.js
+++ server/database/pgdb.js
@@ -3,8 +3,8 @@ const Pool = require('pg').Pool;
 
 // Create a new Pool object to manage a connection pool to the PostgreSQL database
 const pool = new Pool({
-    user: "qap3",        // The PostgreSQL user to connect as
-    host: 'localhost',   // The hostname of the PostgreSQL database server
+    user: "postgres",        // The PostgreSQL user to connect as
+    host: '34.168.13.106',   // The hostname of the PostgreSQL database server
     password: "1234",    // The password to use when connecting to the PostgreSQL server
     database: "sprint02", // The name of the PostgreSQL database to use
     port: 5432,          // The port number that the PostgreSQL server is listening on
