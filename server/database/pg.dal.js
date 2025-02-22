547e3a06499337906da2d3297d88cb0f20a0b578
Cody Fisher <fishercody23@gmail.com>
2025-02-22 01:58:35 -0700

    added the railway postgresql database

--- server/database/pg.dal.js
+++ server/database/pg.dal.js
@@ -9,22 +9,22 @@ var getSearch = async function(search, searchby) {
 
   // Based on the selected search category, construct a SQL query to retrieve matching records from the mock_data table in the PostgreSQL database
   if(searchby == "firstname") {
-    const sql = "SELECT * FROM mock_data WHERE first_name LIKE  INITCAP($1)"
+    const sql = "SELECT * FROM clients WHERE firstname LIKE  INITCAP($1)"
     const results = await dal.query(sql, [fuzzy]);
     return results.rows;
 
   } else if (searchby == "lastname") {
-    const sql = "SELECT * FROM mock_data WHERE last_name LIKE INITCAP($1)"
+    const sql = "SELECT * FROM clients WHERE lastname LIKE INITCAP($1)"
     const results = await dal.query(sql, [fuzzy]);
     return results.rows;
 
   } else if (searchby == "email") {
-    const sql = "SELECT * FROM mock_data WHERE email LIKE $1"
+    const sql = "SELECT * FROM clients WHERE email LIKE $1"
     const results = await dal.query(sql, [fuzzy]);
     return results.rows;
 
   } else {
-    const sql = "SELECT * FROM mock_data WHERE occupation LIKE INITCAP($1)"
+    const sql = "SELECT * FROM clients WHERE occupation LIKE INITCAP($1)"
     const results = await dal.query(sql, [fuzzy]);
     return results.rows;
   }
--- server/database/pgdb.js
+++ server/database/pgdb.js
@@ -4,10 +4,10 @@ const Pool = require('pg').Pool;
 // Create a new Pool object to manage a connection pool to the PostgreSQL database
 const pool = new Pool({
     user: "postgres",        // The PostgreSQL user to connect as
-    host: '34.168.13.106',   // The hostname of the PostgreSQL database server
-    password: "1234",    // The password to use when connecting to the PostgreSQL server
-    database: "sprint02", // The name of the PostgreSQL database to use
-    port: 5432,          // The port number that the PostgreSQL server is listening on
+    host: 'caboose.proxy.rlwy.net',   // The hostname of the PostgreSQL database server
+    password: "oouVDNcPqEuLxiSUTAlXyiZYFNyTYJhx",    // The password to use when connecting to the PostgreSQL server
+    database: "railway", // The name of the PostgreSQL database to use
+    port: 11341,          // The port number that the PostgreSQL server is listening on
 });
 
 // Export the pool object to make it available to other modules
